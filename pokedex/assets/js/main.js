6d52eb2d006f1702ff3e4a57b25efd94dae17944
Guilherme Cruz <67871173+cruz-g-m@users.noreply.github.com>
2022-11-16 08:08:12 -0300

    Trabalhando com JS

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,2 +1,39 @@
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offse=${offset}&limit=${limit}`;
 
-console.log('Sucesso!')
+function convertPokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span> 
+    
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+        alt="${pokemon.name}">
+
+    </div>
+
+</li>
+`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+fetch(url)
+    .then((response) => response.json()) // pedido de converção do body para json
+    .then((jsonBody) => jsonBody.results) //pegando do json a lista de results
+    .then((pokemons) => {
+       
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            
+        }
+    })
+    .catch((error) => console.log(error))
